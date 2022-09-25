b0456cc7dab582f31a2010f700617b6fc1e79330
stars1r <114310588+stars1r@users.noreply.github.com>
2022-09-25 16:45:07 +0800

    init

---
+++ Hello Wold.py
@@ -0,0 +1,23 @@
+import const
+#try to ptinr words
+first_name = "ada"
+
+last_name = "lovelace"
+
+const.my_name = "sparrow"
+
+const.my_name = "Jack"
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name.title())
+#try to delete space
+message = "      ada\nlovalace".title()
+
+print(message.lstrip())
+#try to run math
+message = 3+2
+
+print(message*2)
+
+import this
